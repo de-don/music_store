a9e391791d7a261acaf320cb0fbc7062f2afe410
DenisDontsov <denis.dontsov@saritasa.com>
2018-03-07 11:26:34 +0700

    very short variant

--- music_store/views.py
+++ music_store/views.py
@@ -1,45 +1,19 @@
 from music_store.models import ListenerUser
 from music_store.serializers import ListenerUserSerializer
-from django.http import Http404
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
-from rest_framework import mixins
 from rest_framework import generics
 
 
-class ListenerList(mixins.ListModelMixin,
-                   mixins.CreateModelMixin,
-                   generics.GenericAPIView):
+class ListenerList(generics.ListCreateAPIView):
     """
     List all listeners, or create a new.
     """
     queryset = ListenerUser.objects.all()
     serializer_class = ListenerUserSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-
-class ListenerDetail(mixins.RetrieveModelMixin,
-                     mixins.UpdateModelMixin,
-                     mixins.DestroyModelMixin,
-                     generics.GenericAPIView):
+class ListenerDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     Retrieve, update or delete a code snippet.
     """
     queryset = ListenerUser.objects.all()
     serializer_class = ListenerUserSerializer
-
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
