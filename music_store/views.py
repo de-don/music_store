0d7a84a52b38df6240c38288cc48bceadd67fb21
DenisDontsov <denis.dontsov@saritasa.com>
2018-03-07 11:23:15 +0700

    added mixins

--- music_store/views.py
+++ music_store/views.py
@@ -4,51 +4,42 @@ from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import mixins
+from rest_framework import generics
 
 
-class ListenerList(APIView):
+class ListenerList(mixins.ListModelMixin,
+                   mixins.CreateModelMixin,
+                   generics.GenericAPIView):
     """
     List all listeners, or create a new.
     """
+    queryset = ListenerUser.objects.all()
+    serializer_class = ListenerUserSerializer
 
-    def get(self, request, format=None):
-        lus = ListenerUser.objects.all()
-        serializer = ListenerUserSerializer(lus, many=True)
-        return Response(serializer.data)
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
 
-    def post(self, request, format=None):
-        serializer = ListenerUserSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
 
 
-class ListenerDetail(APIView):
+class ListenerDetail(mixins.RetrieveModelMixin,
+                     mixins.UpdateModelMixin,
+                     mixins.DestroyModelMixin,
+                     generics.GenericAPIView):
     """
     Retrieve, update or delete a code snippet.
     """
+    queryset = ListenerUser.objects.all()
+    serializer_class = ListenerUserSerializer
 
-    def get_object(self, pk):
-        try:
-            return ListenerUser.objects.get(pk=pk)
-        except ListenerUser.DoesNotExist:
-            raise Http404
 
-    def get(self, request, pk, format=None):
-        lu = self.get_object(pk)
-        serializer = ListenerUserSerializer(lu)
-        return Response(serializer.data)
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
 
-    def put(self, request, pk, format=None):
-        lu = self.get_object(pk)
-        serializer = ListenerUserSerializer(lu, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
 
-    def delete(self, request, pk, format=None):
-        lu = self.get_object(pk)
-        lu.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
